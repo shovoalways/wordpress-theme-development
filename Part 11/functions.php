ebb26e8e24e3d1d5dfd432f8eb5cfb6df3f76b97
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2021-10-02 19:24:06 +0600

    Add files via upload

---
+++ Part 11/functions.php
@@ -0,0 +1,112 @@
+<?php
+/*
+* My Theme Function
+*/
+
+
+// Theme Title
+add_theme_support('title-tag');
+
+
+// Theme CSS and jQuery File calling
+function ali_css_js_file_calling(){
+  wp_enqueue_style('ali-style', get_stylesheet_uri());
+  wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all');
+  wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+  wp_enqueue_style('bootstrap');
+  wp_enqueue_style('custom');
+
+
+  //jQuery
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.0.2', 'true' );
+  wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true' );
+
+}
+add_action('wp_enqueue_scripts', 'ali_css_js_file_calling');
+
+
+// Google Fonts Enqueue
+function ali_add_google_fonts(){
+  wp_enqueue_style('ali_google_fonts', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald&display=swap', false);
+}
+add_action('wp_enqueue_scripts', 'ali_add_google_fonts');
+
+
+//Theme Function
+function ali_customizar_register($wp_customize){
+
+  //Header Area Function
+  $wp_customize->add_section('ali_header_area', array(
+    'title' =>__('Header Area', 'alihossain'),
+    'description' => 'If you interested to update your header area, you can do it here.'
+  ));
+
+  $wp_customize->add_setting('ali_logo', array(
+    'default' => get_bloginfo('template_directory') . '/img/logo.png',
+  ));
+
+  $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'ali_logo', array(
+    'label' => 'Logo Upload',
+    'description' => 'If you interested to change or update your logo you can do it.',
+    'setting' => 'ali_logo',
+    'section' => 'ali_header_area',
+  ) ));
+
+  // Menu Position Option
+  $wp_customize->add_section('ali_menu_option', array(
+    'title' => __('Menu Position Option', 'alihossain'),
+    'description' => 'If you interested to change your menu position you can do it.'
+  ));
+
+  $wp_customize->add_setting('ali_menu_position', array(
+    'default' => 'right_menu',
+  ));
+
+  $wp_customize-> add_control('ali_menu_position', array(
+    'label' => 'Menu Position',
+    'description' => 'Select your menu position',
+    'setting' => 'ali_menu_position',
+    'section' => 'ali_menu_option',
+    'type' => 'radio',
+    'choices' => array(
+      'left_menu' => 'Left Menu',
+      'right_menu' => 'Right Menu',
+      'center_menu' => 'Center Menu',
+    ),
+  ));
+
+
+  // Footer Option
+  $wp_customize->add_section('ali_footer_option', array(
+    'title' => __('Footer Option', 'alihossain'),
+    'description' => 'If you interested to change or update your footer settings you can do it.'
+  ));
+
+  $wp_customize->add_setting('ali_copyright_section', array(
+    'default' => '&copy; Copyright 2021 | Procoder BD',
+  ));
+
+  $wp_customize-> add_control('ali_copyright_section', array(
+    'label' => 'Copyright Text',
+    'description' => 'If need you can update your copyright text from here',
+    'setting' => 'ali_copyright_section',
+    'section' => 'ali_footer_option',
+  ));
+
+}
+
+add_action('customize_register', 'ali_customizar_register');
+
+
+// Menu Register
+register_nav_menu( 'main_menu', __('Main Menu', 'alihossain') );
+
+// Walker Menu Properties
+function ali_nav_description( $item_output, $item, $args){
+  if( !empty ($item->description)){
+    $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' . $item->description . '</span>' . $args->link_after . '</a>', $item_output);
+  }
+  return $item_output;
+}
+add_filter('walker_nav_menu_start_el', 'ali_nav_description', 10, 3);
