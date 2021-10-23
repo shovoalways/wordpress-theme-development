b8f76bbf6f86c23cbe7c0a4bcdb18c02b96f05fb
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2021-10-23 19:20:58 +0600

    Add files via upload

---
+++ Part 16/index.php
@@ -0,0 +1,41 @@
+<?php
+/*
+* The main template file
+*/ 
+get_header(); ?>
+
+  <section id="body_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
+          <?php if (have_posts()) :
+              while (have_posts()) : the_post(); ?>
+
+              <div class="blog_area">
+                  <div class="post_thumb">
+                    <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
+                  </div>
+                  <div class="post_details">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php the_excerpt(); ?>
+                  </div>
+              </div>
+          <?php endwhile;
+            else :
+              _e('No post found');
+            endif; ?>
+          <div id="page_nav">
+            <?php if ('ali_pagenav') {ali_pagenav(); } else{ ?>
+                <?php next_posts_link(); ?>
+                <?php previous_posts_link(); ?>
+            <?php } ?>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <h2>This is sidebar area</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php get_footer(); ?>
