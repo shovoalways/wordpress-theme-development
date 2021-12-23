cf58a65c3f8b046c2e720d1c688615b9accf86b8
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2021-12-23 20:25:11 +0600

    Add files via upload

---
+++ Part 24 - 25/inc/custom_post.php
@@ -0,0 +1,34 @@
+<?php
+
+function custom_service(){
+  register_post_type ('service',
+    array(
+      'labels' => array(
+        'name' => ('Services'),
+        'singular_name' => ('Service'),
+        'add_new' => ('Add New Service'),
+        'add_new_item' => ('Add New Service'),
+        'edit_item' => ('Edit Service'),
+        'new_item' => ('New Service'),
+        'view_item' => ('View Service'),
+        'not_found' => ('Sorry, we cound\'n find the service you are looking for.'),
+      ),
+      'menu_icon' => 'dashicons-networking',
+      'public' => true,
+      'publicly_queryable' => true,
+      'exclude_from_search' => true,
+      'menu_position' => 5, 
+      'has_archive' => true,
+      'hierarchial' => true,
+      'show_ui' => true,
+      'capability_type' => 'post',
+      'rewrite' => array('slag' => 'service'),
+      'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+      )
+    );
+    add_theme_support('post-thumbnails');
+}
+
+add_action('init', 'custom_service');
+
+
---
+++ Part 24 - 25/inc/theme_function.php
@@ -0,0 +1,98 @@
+<?php
+
+function ali_customizar_register($wp_customize){
+
+  //Header Area Function
+  $wp_customize->add_section('ali_header_area', array(
+    'title' =>__('Header Area', 'alihossain'),
+    'description' => 'If you interested to update your header area, you can do it here.'
+  ));
+
+  $wp_customize->add_setting('ali_logo', array(
+    'default' => get_bloginfo('template_directory') . '/img/logo.png',
+  ));
+
+  $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'ali_logo', array(
+    'label' => 'Logo Upload',
+    'description' => 'If you interested to change or update your logo you can do it.',
+    'setting' => 'ali_logo',
+    'section' => 'ali_header_area',
+  ) ));
+
+  // Menu Position Option
+  $wp_customize->add_section('ali_menu_option', array(
+    'title' => __('Menu Position Option', 'alihossain'),
+    'description' => 'If you interested to change your menu position you can do it.'
+  ));
+
+  $wp_customize->add_setting('ali_menu_position', array(
+    'default' => 'right_menu',
+  ));
+
+  $wp_customize-> add_control('ali_menu_position', array(
+    'label' => 'Menu Position',
+    'description' => 'Select your menu position',
+    'setting' => 'ali_menu_position',
+    'section' => 'ali_menu_option',
+    'type' => 'radio',
+    'choices' => array(
+      'left_menu' => 'Left Menu',
+      'right_menu' => 'Right Menu',
+      'center_menu' => 'Center Menu',
+    ),
+  ));
+
+
+  // Footer Option
+  $wp_customize->add_section('ali_footer_option', array(
+    'title' => __('Footer Option', 'alihossain'),
+    'description' => 'If you interested to change or update your footer settings you can do it.'
+  ));
+
+  $wp_customize->add_setting('ali_copyright_section', array(
+    'default' => '&copy; Copyright 2021 | Procoder BD',
+  ));
+
+  $wp_customize-> add_control('ali_copyright_section', array(
+    'label' => 'Copyright Text',
+    'description' => 'If need you can update your copyright text from here',
+    'setting' => 'ali_copyright_section',
+    'section' => 'ali_footer_option',
+  ));
+
+  // Theme Color
+  $wp_customize-> add_section('ali_colors', array(
+    'title' => __('Theme Color', 'alihossain'),
+    'description' => 'If need you can change your theme color.',
+  ));
+
+  $wp_customize ->add_setting('ali_bg_color', array(
+    'default' => '#ffffff',
+  ));
+  $wp_customize->add_control( new WP_Customize_color_control($wp_customize, 'ali_bg_color', array(
+    'label' => 'Background Color',
+    'section' => 'ali_colors',
+    'settings' => 'ali_bg_color',
+  )));
+  $wp_customize ->add_setting('ali_primary_color', array(
+    'default' => '#ea1a70',
+  ));
+  $wp_customize->add_control( new WP_Customize_color_control($wp_customize, 'ali_primary_color', array(
+    'label' => 'Primary Color',
+    'section' => 'ali_colors',
+    'settings' => 'ali_primary_color',
+  )));
+
+}
+
+add_action('customize_register', 'ali_customizar_register');
+
+function ali_theme_color_cus(){
+  ?>
+  <style>
+    body{background: <?php echo get_theme_mod('ali_bg_color'); ?>}
+    :root{ --pink:<?php echo get_theme_mod('ali_primary_color'); ?>}
+  </style>
+  <?php 
+}
+add_action('wp_head', 'ali_theme_color_cus');
