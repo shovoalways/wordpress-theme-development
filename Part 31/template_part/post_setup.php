4528e9476ff6631682463e983648ed03d64cb047
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2022-02-24 23:33:44 +0600

    Add files via upload

---
+++ Part 31/template_part/post_setup.php
@@ -0,0 +1,16 @@
+          <?php if (have_posts()) :
+              while (have_posts()) : the_post(); ?>
+
+              <div class="blog_area">
+                  <div class="post_thumb">
+                    <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
+                  </div>
+                  <div class="post_details">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php the_content(); ?>
+                  </div>
+              </div>
+          <?php endwhile;
+            else :
+              _e('No post found');
+            endif; ?>
