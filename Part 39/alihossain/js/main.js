5be503b28ddb12ccbc74d3b01baac1e43bb800c7
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2022-10-22 21:03:28 +0600

    Error Fixed
    Some minor errors fixed as well as "Theme Check" plugin was introduced and fixed all required errors.

---
+++ Part 39/alihossain/js/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+  jQuery(".slider").bxSlider({
+    mode: 'fade',
+    captions: true,
+  });
+});
---
+++ Part 39/alihossain/single.php
@@ -0,0 +1,24 @@
+<?php
+/*
+* The template for displaying pages
+*/ 
+get_header(); ?>
+
+  <section id="body_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9 post_page">
+            <?php get_template_part('template_part/post_setup'); ?>
+
+            <div id="comments_area" <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">>
+              <?php comments_template(); ?>
+            </div>
+        </div>
+        <div class="col-md-3">
+          <?php get_sidebar(); ?>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php get_footer(); ?>
