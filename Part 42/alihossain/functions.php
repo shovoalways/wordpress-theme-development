67d27a9066bc549e0f1fae7d2dc9c6c0613e6d5a
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2022-12-02 15:07:37 +0600

    Login page custom design
    Login page custom design

---
+++ Part 42/alihossain/functions.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* My Theme Function
+*/
+
+// ALl Default theme function here
+include_once('inc/default.php');
+
+
+// Adding Custom post
+include_once('inc/custom_post.php');
+
+// Theme CSS and jQuery File calling
+include_once('inc/enqueue.php');
+
+
+//Theme Function
+include_once('inc/theme_function.php');
+
+
+// Menu Register
+include_once('inc/menu_register.php');
+
+
+// Widgets Register
+include_once('inc/widgets_register.php');
+
+
+// Shortcode Register
+include_once('inc/shortcode.php');
+
+
+// Login Enqueue Register
+include_once('inc/login-enqueue.php');
+
